031e0309981c838e04c381920f52a0080a37b0be
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-07-26 18:43:00 +0700

    Menambah fungsi login

--- app/src/main/java/com/azhara/inventarisbarang/auth/login/LoginFragment.kt
+++ app/src/main/java/com/azhara/inventarisbarang/auth/login/LoginFragment.kt
@@ -5,30 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.azhara.inventarisbarang.R
+import com.azhara.inventarisbarang.auth.viewmodel.AuthViewModel
+import es.dmoral.toasty.Toasty
+import kotlinx.android.synthetic.main.fragment_login.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class LoginFragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var authViewModel: AuthViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +26,71 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        authViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[AuthViewModel::class.java]
+        btn_login.setOnClickListener(this)
+        btn_to_register.setOnClickListener(this)
+        tv_forgot_password.setOnClickListener(this)
     }
+
+    private fun login(email: String, password: String){
+        authViewModel.login(email, password)
+
+        authViewModel.loginState().observe(viewLifecycleOwner, Observer { loginState ->
+            loading(false)
+            if (loginState == true){
+                context?.let { Toasty.success(it, "Success Login", Toasty.LENGTH_LONG, true).show() }
+            }else{
+                context?.let { Toasty.error(it, "${authViewModel.loginMessage}", Toast.LENGTH_LONG, true).show() }
+            }
+        })
+    }
+
+    private fun checkInput(){
+        loading(true)
+        val email = edt_email_login.text.toString().trim()
+        val password = edt_password_login.text.toString().trim()
+
+        if (email.isEmpty()){
+            loading(false)
+            input_layout_email_login.error = "Email tidak boleh kosong!"
+            return
+        }
+
+        if (password.isEmpty()){
+            loading(false)
+            input_layout_password_login.error = "Password tidak boleh kosong!"
+            return
+        }
+
+        if (email.isNotEmpty() && password.isNotEmpty()){
+            login(email, password)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_login -> {
+                input_layout_email_login.error = null
+                input_layout_password_login.error = null
+                checkInput()
+            }
+            R.id.btn_to_register -> {
+                view?.findNavController()?.navigate(R.id.action_navigation_login_fragment_to_navigation_register_fragment)
+            }
+            R.id.tv_forgot_password -> {
+                view?.findNavController()?.navigate(R.id.action_navigation_login_fragment_to_navigation_reset_fragment)
+            }
+        }
+    }
+
+    private fun loading(state: Boolean){
+        if (state){
+            loading_login.visibility = View.VISIBLE
+        }else{
+            loading_login.visibility = View.INVISIBLE
+        }
+    }
+
 }
--- app/src/main/java/com/azhara/inventarisbarang/auth/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/azhara/inventarisbarang/auth/viewmodel/AuthViewModel.kt
@@ -1,2 +1,30 @@
 package com.azhara.inventarisbarang.auth.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthViewModel : ViewModel(){
+
+    private val auth = FirebaseAuth.getInstance()
+    private val loginState = MutableLiveData<Boolean>()
+    private val tag = AuthViewModel::class.java.simpleName
+    var loginMessage: String? = null
+
+    fun login(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                loginState.postValue(true)
+            }else{
+                Log.e(tag, "Error login: ${task.exception}")
+                loginMessage = "${task.exception?.message}"
+                loginState.postValue(false)
+            }
+        }
+    }
+
+    fun loginState(): LiveData<Boolean> = loginState
+}
+
