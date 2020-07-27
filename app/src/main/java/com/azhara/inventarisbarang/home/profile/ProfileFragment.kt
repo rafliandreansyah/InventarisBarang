b92681c4f4d7699d514310fa6825fc1bd359a949
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-07-27 14:21:54 +0700

    Load data profile pada ProfileFragment.kt dan memberi navigasi pada setiap button

--- app/src/main/java/com/azhara/inventarisbarang/home/profile/ProfileFragment.kt
+++ app/src/main/java/com/azhara/inventarisbarang/home/profile/ProfileFragment.kt
@@ -1,34 +1,27 @@
 package com.azhara.inventarisbarang.home.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.azhara.inventarisbarang.R
+import com.azhara.inventarisbarang.auth.AuthActivity
+import com.azhara.inventarisbarang.home.HomeActivity
+import com.azhara.inventarisbarang.home.profile.viewmodel.ProfileViewModel
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import es.dmoral.toasty.Toasty
+import kotlinx.android.synthetic.main.fragment_profile.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ProfileFragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var profileViewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +31,67 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AccountFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_to_edit_profile.setOnClickListener(this)
+        btn_to_change_password.setOnClickListener(this)
+        btn_logout.setOnClickListener(this)
+        back_button.setOnClickListener(this)
+        profileViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[ProfileViewModel::class.java]
+        dataUser()
+    }
+
+    private fun dataUser(){
+        loading(true)
+        profileViewModel.loadDataUser()
+
+        profileViewModel.dataUser().observe(viewLifecycleOwner, Observer { data->
+            if (data != null){
+                loading(false)
+                if (data.imgUrl != null){
+                    context?.let { Glide.with(it).load(data.imgUrl).into(img_profile) }
                 }
+                tv_name_profile.text = "${data.name}"
+                tv_email_profile.text = "${data.email}"
+                tv_phone_profile.text = "${data.telephone}"
+                tv_position_profile.text = "${data.position}"
+            }else{
+                loading(false)
+                context?.let { Toasty.error(it, "Error load data", Toast.LENGTH_LONG, true).show() }
             }
+        })
+    }
+
+    private fun loading(state: Boolean){
+        if (state){
+            loading_profile.visibility = View.VISIBLE
+        }else{
+            loading_profile.visibility = View.INVISIBLE
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_to_edit_profile -> {
+                view?.findNavController()?.navigate(R.id.action_navigation_profile_fragment_to_navigation_edit_profile_fragment)
+            }
+            R.id.btn_to_change_password -> {
+                view?.findNavController()?.navigate(R.id.action_navigation_profile_fragment_to_navigation_change_password_fragment)
+            }
+            R.id.btn_logout -> {
+                signOut()
+            }
+            R.id.back_button -> {
+                activity?.onBackPressed()
+            }
+        }
+    }
+
+    private fun signOut(){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(Intent(context, AuthActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        })
+        activity?.finish()
     }
 }
--- app/src/main/java/com/azhara/inventarisbarang/home/profile/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/azhara/inventarisbarang/home/profile/viewmodel/ProfileViewModel.kt
@@ -1,2 +1,34 @@
 package com.azhara.inventarisbarang.home.profile.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.azhara.inventarisbarang.entity.User
+
+class ProfileViewModel : ViewModel(){
+
+    private val auth = FirebaseAuth.getInstance()
+    private val db = FirebaseFirestore.getInstance()
+    private val userId = auth.currentUser?.uid
+    private val tag = ProfileViewModel::class.java.simpleName
+    private val dataUser = MutableLiveData<User>()
+
+    fun loadDataUser(){
+        val userDb = db.collection("users").document("$userId")
+        userDb.addSnapshotListener { value, error ->
+            if (error != null){
+                Log.e(tag, "Error load data user: ${error.message}")
+            }
+            if (value != null && value.exists()){
+                val data = value.toObject(User::class.java)
+                dataUser.postValue(data)
+            }
+        }
+    }
+
+    fun dataUser(): LiveData<User> = dataUser
+
+}
