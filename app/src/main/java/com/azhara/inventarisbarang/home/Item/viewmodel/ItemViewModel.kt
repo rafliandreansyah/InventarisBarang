4fd0ca6b15e51ec215c98a2418570131f9c9313a
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-07-28 19:52:07 +0700

    Menambah fungsi load data Barang

--- app/src/main/java/com/azhara/inventarisbarang/home/Item/viewmodel/ItemViewModel.kt
+++ app/src/main/java/com/azhara/inventarisbarang/home/Item/viewmodel/ItemViewModel.kt
@@ -1,2 +1,42 @@
 package com.azhara.inventarisbarang.home.Item.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.azhara.inventarisbarang.entity.Product
+import com.google.firebase.firestore.FirebaseFirestore
+
+class ItemViewModel : ViewModel(){
+
+    private val db = FirebaseFirestore.getInstance()
+    private val tag = ItemViewModel::class.java.simpleName
+    private val productData = MutableLiveData<List<Product>>()
+
+    fun getDataProduct(){
+        val productDb = db.collection("product")
+
+        productDb.addSnapshotListener { value, error ->
+            if (error != null){
+                Log.e(tag, "Error load data product: ${error.message}")
+            }
+
+            if (value != null){
+                val listProduct = ArrayList<Product>()
+                val data = value.documents
+                data.forEach {
+                    val product = it.toObject(Product::class.java)
+                    if (product != null){
+                        product.productId = it.id
+                        listProduct.add(product)
+                    }
+                }
+                productData.postValue(listProduct)
+                Log.d("product", "$listProduct")
+            }
+        }
+    }
+
+    fun productData() : LiveData<List<Product>> = productData
+}
+
